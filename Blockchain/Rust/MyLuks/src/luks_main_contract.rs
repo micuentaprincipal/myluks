a8a3ec612a3af484707cc8145c770edcd10546a5
SirPotato27 <141689212+SirPotato27@users.noreply.github.com>
2023-09-03 14:13:57 -0500

    Update luks_main_contract.rs
    Luks_main_contract.rs code update 03/09/2023

--- Blockchain/Rust/MyLuks/src/luks_main_contract.rs
+++ Blockchain/Rust/MyLuks/src/luks_main_contract.rs
@@ -1,4 +1,4 @@
-// Declarando módulos
+// Importing necessary modules
 mod accounts;
 mod transfer;
 mod validate_transaction;
@@ -8,68 +8,75 @@ mod security;
 
 use std::collections::HashMap;
 
+// Enum for error handling
+pub enum LuksError {
+    AccountAlreadyExists,
+    AccountDeletionError,
+    AccountNotFound,
+    InsufficientBalance,
+    InvalidTransaction,
+    Unauthorized
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct Address {
-    pub value: String, // Representa la dirección en la blockchain.
+    pub value: String, // Represents the address on the blockchain.
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Account {
     pub balance: u64,
-    // Otros detalles de la cuenta podrían ir aquí (por ejemplo, llaves públicas, historial de transacciones, etc.)
+    // Additional details could go here (public keys, transaction history, etc.)
 }
 
 #[derive(Debug, PartialEq)]
 pub struct LuksMainContract {
     pub owner: Address,
     pub accounts: HashMap<Address, Account>,
+    // Adding a consensus property
+    pub consensus_module: consensus::Consensus,
 }
 
 impl LuksMainContract {
-    pub fn create_account(&mut self, address: Address, initial_balance: u64) -> Result<(), &'static str> {
+    // Function to create a new account
+    pub fn create_account(&mut self, address: Address, initial_balance: u64) -> Result<(), LuksError> {
         if self.accounts.contains_key(&address) {
-            return Err("La cuenta ya existe");
+            return Err(LuksError::AccountAlreadyExists);
         }
         self.accounts.insert(address, Account { balance: initial_balance });
         Ok(())
     }
 
-    pub fn delete_account(&mut self, address: &Address) -> Result<(), &'static str> {
-        self.accounts.remove(address).ok_or("Error al eliminar la cuenta")?;
+    // Function to delete an account
+    pub fn delete_account(&mut self, address: &Address, caller: &Address) -> Result<(), LuksError> {
+        if &self.owner != caller {
+            return Err(LuksError::Unauthorized);
+        }
+        self.accounts.remove(address).ok_or(LuksError::AccountDeletionError)?;
         Ok(())
     }
 
+    // Function to get account details
     pub fn get_account_details(&self, address: &Address) -> Option<&Account> {
         self.accounts.get(address)
     }
 
-    pub fn initiate_transfer(&mut self, from: &Address, to: &Address, amount: u64) -> Result<(), &'static str> {
-        // Validamos la transacción antes de procesarla
-        let is_valid = self.validate_transaction(from, to, amount)?;
+    // Function to initiate a transfer
+    pub fn initiate_transfer(&mut self, from: &Address, to: &Address, amount: u64) -> Result<(), LuksError> {
+        let is_valid = validate_transaction::validate(from, to, amount)?;
         if !is_valid {
-            return Err("Transacción no válida");
+            return Err(LuksError::InvalidTransaction);
         }
         
-        let sender = self.accounts.get_mut(from).ok_or("Cuenta del remitente no encontrada")?;
-        let receiver = self.accounts.get_mut(to).ok_or("Cuenta del destinatario no encontrada")?;
+        let sender = self.accounts.get_mut(from).ok_or(LuksError::AccountNotFound)?;
+        let receiver = self.accounts.get_mut(to).ok_or(LuksError::AccountNotFound)?;
         
         if sender.balance < amount {
-            return Err("Saldo insuficiente");
+            return Err(LuksError::InsufficientBalance);
         }
         sender.balance -= amount;
         receiver.balance += amount;
 
         Ok(())
-    }
-
-    pub fn validate_transaction(&self, from: &Address, to: &Address, amount: u64) -> Result<bool, &'static str> {
-        // Llamamos al módulo validate_transaction para validar la transacción
-        // Aquí es donde se verificarán las firmas, el doble gasto, etc.
-        // Para este ejemplo, sólo comprobamos el saldo
-        let sender = self.accounts.get(from).ok_or("Cuenta del remitente no encontrada")?;
-        if sender.balance < amount {
-            return Err("Saldo insuficiente");
-        }
-        Ok(true)
     }
 }
