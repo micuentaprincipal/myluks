010a612758c22cebd2fb4e9076174cd5459e8b57
SirPotato27 <141689212+SirPotato27@users.noreply.github.com>
2023-09-03 14:15:44 -0500

    Update security.rs
    Security.rs code update 03/09/2023

--- Blockchain/Rust/MyLuks/src/security.rs
+++ Blockchain/Rust/MyLuks/src/security.rs
@@ -1,15 +1,26 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
+use sha2::{Sha256, Digest};
 
-/// Representa las utilidades de seguridad para la blockchain.
+/// Security module for handling transaction processing and cryptographic operations.
+/// Includes features like signing, validation, and key management.
+pub enum SecurityError {
+    TransactionAlreadyProcessed,
+    InvalidSignature,
+    KeyNotFound,
+    FailedToSign,
+    InvalidKey, // New error type for invalid keys
+}
+
+/// Main structure for the security module.
 pub struct Security {
     processed_transactions: HashSet<String>,
-    // Suponiendo que almacenamos las claves públicas y privadas en mapas para la simulación.
+    // Simulated storage for public and private keys.
     public_keys: HashMap<String, Vec<u8>>,
     private_keys: HashMap<String, Vec<u8>>,
 }
 
 impl Security {
-    /// Crea una nueva instancia de `Security`.
+    /// Constructor for initializing a new Security instance.
     pub fn new() -> Self {
         Security {
             processed_transactions: HashSet::new(),
@@ -18,51 +29,61 @@ impl Security {
         }
     }
 
-    /// Añade una transacción procesada al conjunto.
-    /// Esto ayuda a garantizar que no se procese una transacción más de una vez.
-    pub fn add_processed_transaction(&mut self, transaction_hash: String) -> Result<(), &'static str> {
-        if self.processed_transactions.contains(&transaction_hash) {
-            return Err("La transacción ya ha sido añadida");
+    /// Adds a processed transaction to the set.
+    pub fn add_processed_transaction(&mut self, transaction_hash: String) -> Result<(), SecurityError> {
+        if !self.processed_transactions.insert(transaction_hash) {
+            return Err(SecurityError::TransactionAlreadyProcessed);
         }
-        
-        self.processed_transactions.insert(transaction_hash);
         Ok(())
     }
 
-    /// Verifica y previene el doble gasto al comprobar si una transacción ya ha sido procesada.
-    pub fn prevent_double_spend(&mut self, transaction_hash: String) -> Result<(), &'static str> {
-        if self.processed_transactions.contains(&transaction_hash) {
-            return Err("La transacción ya ha sido procesada");
-        }
-        
-        self.processed_transactions.insert(transaction_hash);
-        Ok(())
-    }
-
-    /// Comprueba si una transacción ya ha sido procesada.
+    /// Checks if a transaction has already been processed.
     pub fn has_transaction_been_processed(&self, transaction_hash: &String) -> bool {
         self.processed_transactions.contains(transaction_hash)
     }
 
-    /// Simula la protección de claves privadas y la integridad de la red.
-    pub fn protect_private_keys_and_network_integrity(&self) {
-        // Esta es una simulación y no debe considerarse una implementación real de seguridad.
-        println!("Ejecutando protocolos de seguridad para claves privadas y verificando la integridad de la red...");
+    /// Simulates signing using a "private key".
+    pub fn sign(&self, account: &String, data: &String) -> Result<String, SecurityError> {
+        let private_key = self.private_keys.get(account).ok_or(SecurityError::KeyNotFound)?;
+        let hashed_data = self.hash(data);
+        if &hashed_data == private_key {
+            Ok(hashed_data)
+        } else {
+            Err(SecurityError::FailedToSign)
+        }
     }
 
-    /// Simula la validación de firmas utilizando claves públicas.
-    pub fn validate_signature(&self, account: &String, signature: &String) -> bool {
-        // Esta es una validación simplificada: simplemente comprobamos que la firma sea igual a la clave pública del remitente.
-        let public_key = self.public_keys.get(account).unwrap_or(&vec![]);
-        public_key == &signature.as_bytes()
+    /// Simulates signature validation using a "public key".
+    pub fn validate_signature(&self, account: &String, data: &String, signature: &String) -> Result<bool, SecurityError> {
+        let public_key = self.public_keys.get(account).ok_or(SecurityError::KeyNotFound)?;
+        let hashed_data = self.hash(data);
+        if &hashed_data == public_key && &hashed_data == signature {
+            Ok(true)
+        } else {
+            Err(SecurityError::InvalidSignature)
+        }
     }
 
-    /// Añade claves públicas y privadas a los mapas para la simulación.
-    pub fn add_keys(&mut self, account: String, public_key: Vec<u8>, private_key: Vec<u8>) {
+    /// Validates keys before adding them to storage. (New function)
+    fn validate_keys(public_key: &Vec<u8>, private_key: &Vec<u8>) -> Result<(), SecurityError> {
+        if public_key.is_empty() || private_key.is_empty() {
+            return Err(SecurityError::InvalidKey);
+        }
+        Ok(())
+    }
+
+    /// Adds public and private keys to the simulated storage.
+    pub fn add_keys(&mut self, account: String, public_key: Vec<u8>, private_key: Vec<u8>) -> Result<(), SecurityError> {
+        Self::validate_keys(&public_key, &private_key)?;
         self.public_keys.insert(account.clone(), public_key);
         self.private_keys.insert(account, private_key);
+        Ok(())
+    }
+    
+    /// Simulates a hashing process.
+    fn hash(&self, data: &String) -> String {
+        let mut hasher = Sha256::new();
+        hasher.update(data);
+        format!("{:x}", hasher.finalize())
     }
 }
-
-// Nota: La estructura y las funciones proporcionadas son un esqueleto y sirven como punto de partida.
-// En la implementación real, se necesitaría una lógica de seguridad más detallada y robusta.
