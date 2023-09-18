920b9dc609c6fb72c6e42eef48bb932136237c0b
SirPotato27 <141689212+SirPotato27@users.noreply.github.com>
2023-09-17 22:20:36 -0500

    Update coin_issue.rs
    Code update 17/09/2023

--- Blockchain/Rust/MyLuks/src/coin_issue.rs
+++ Blockchain/Rust/MyLuks/src/coin_issue.rs
@@ -1,78 +1,100 @@
 use std::collections::HashSet;
+use std::time::Instant; // Para medir el rendimiento
+use crypto::digest::Digest; // Para hash verification
+use crypto::sha2::Sha256; // Para hash verification
 
-/// Enum for Coin Issue errors.
 pub enum CoinIssueError {
     TransactionAlreadyProcessed,
-    MaxSupplyReached, // New error type for maximum supply limit
-    InvalidAddress,   // New error type for invalid central bank address
+    MaxSupplyReached,
+    InvalidAddress,
+    NotAuthorized,
+    IntegrityViolation, // Nuevo: Para verificación de hash
 }
 
-/// Representa la emisión y gestión de monedas en la blockchain.
 pub struct CoinIssue {
     total_supply: u64,
     reward_per_block: u64,
     inflation_rate: u64,
-    max_supply: u64, // New field to specify maximum supply limit
+    max_supply: u64,
     central_bank: String,
-    emitted_transactions: HashSet<String>, // Registra las transacciones de emisión
+    emitted_transactions: HashSet<String>,
+    accounts_hash: String, // Nuevo: Para la verificación de hash
 }
 
 impl CoinIssue {
-    /// Crea una nueva instancia de `CoinIssue`.
-    pub fn new(initial_supply: u64, reward_per_block: u64, inflation_rate: u64, max_supply: u64, central_bank: String) -> Self {
+    const INITIAL_HASH: &'static str = "some_real_initial_hash_value"; 
+
+    pub fn new(
+        initial_supply: u64,
+        reward_per_block: u64,
+        inflation_rate: u64,
+        max_supply: u64,
+        central_bank: String,
+    ) -> Self {
         CoinIssue {
             total_supply: initial_supply,
             reward_per_block,
             inflation_rate,
-            max_supply, // Initialize max_supply
+            max_supply,
             central_bank,
             emitted_transactions: HashSet::new(),
+            accounts_hash: Self::INITIAL_HASH.to_string(),
         }
     }
 
-    /// Emite nuevas monedas, ajustando el suministro total en función de la inflación.
-    pub fn issue_coins(&mut self, transaction_id: String) -> Result<u64, CoinIssueError> {
+    pub fn can_issue_coins(&self, requesting_address: &String) -> bool {
+        &self.central_bank == requesting_address
+    }
+
+    fn update_and_verify_hash(&mut self, new_transaction_id: &String) -> bool {
+        let mut hasher = Sha256::new();
+        hasher.input_str(&format!("{}{}", &self.accounts_hash, new_transaction_id));
+        let new_hash = hasher.result_str();
+
+        if new_hash == self.accounts_hash {
+            return false;
+        }
+
+        self.accounts_hash = new_hash;
+        true
+    }
+
+    fn recalculate_reward(&mut self) {
+        self.reward_per_block = self.reward_per_block + (self.total_supply * self.inflation_rate / 100);
+    }
+
+    pub fn issue_coins(
+        &mut self,
+        transaction_id: String,
+        requesting_address: &String,
+    ) -> Result<u64, CoinIssueError> {
+        let start_time = Instant::now(); 
+
+        if !self.can_issue_coins(requesting_address) {
+            return Err(CoinIssueError::NotAuthorized);
+        }
+
+        if !self.update_and_verify_hash(&transaction_id) {
+            return Err(CoinIssueError::IntegrityViolation);
+        }
+
         if self.emitted_transactions.contains(&transaction_id) {
             return Err(CoinIssueError::TransactionAlreadyProcessed);
         }
 
-        let new_coins = self.reward_per_block;
+        self.recalculate_reward();
 
-        if self.total_supply + new_coins > self.max_supply {
+        let emission = self.reward_per_block;
+        if self.total_supply + emission > self.max_supply {
             return Err(CoinIssueError::MaxSupplyReached);
         }
 
-        if self.total_supply != 0 {
-            self.total_supply += new_coins + (self.total_supply * self.inflation_rate / 100);
-        } else {
-            self.total_supply += new_coins;
-        }
-
+        self.total_supply += emission;
         self.emitted_transactions.insert(transaction_id);
-        Ok(new_coins)
-    }
 
-    /// Define las reglas de emisión, permitiendo ajustar la recompensa por bloque y la tasa de inflación.
-    pub fn define_emission_rules(&mut self, new_reward_per_block: u64, new_inflation_rate: u64) -> Result<(), CoinIssueError> {
-        if new_reward_per_block + self.total_supply > self.max_supply {
-            return Err(CoinIssueError::MaxSupplyReached);
-        }
-        self.reward_per_block = new_reward_per_block;
-        self.inflation_rate = new_inflation_rate;
-        Ok(())
-    }
+        let elapsed_time = start_time.elapsed(); 
+        println!("Tiempo tomado para emitir monedas: {:?}", elapsed_time);
 
-    /// Cambia la dirección del banco central.
-    pub fn change_central_bank(&mut self, new_central_bank: String) -> Result<(), CoinIssueError> {
-        if new_central_bank.is_empty() {
-            return Err(CoinIssueError::InvalidAddress);
-        }
-        self.central_bank = new_central_bank;
-        Ok(())
-    }
-
-    /// Obtiene el suministro total de monedas en circulación.
-    pub fn get_total_supply(&self) -> u64 {
-        self.total_supply
+        Ok(emission)
     }
 }
