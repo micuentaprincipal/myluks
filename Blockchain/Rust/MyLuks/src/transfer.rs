f6a2fd0ed5891147722def3fff559417fe7abf8e
SirPotato27 <141689212+SirPotato27@users.noreply.github.com>
2023-09-17 22:33:28 -0500

    Update transfer.rs
    Update code 17/09/2023

--- Blockchain/Rust/MyLuks/src/transfer.rs
+++ Blockchain/Rust/MyLuks/src/transfer.rs
@@ -9,7 +9,7 @@ pub enum TransferError {
 
 use crate::accounts::{Accounts, AccountError, TransactionHistory};
 use std::time::{SystemTime, UNIX_EPOCH};
-use crate::validate_transaction::ValidationError;  // New import for additional validation
+use crate::validate_transaction::ValidationError;
 
 #[derive(Debug, PartialEq)]
 pub enum TransferType {
@@ -24,73 +24,94 @@ pub struct Transfer {
 }
 
 impl Transfer {
-    // Function to validate transfer amount (New Function)
+    // Constantes y estructuras
+    const TRANSFER_FEE_PERCENTAGE: f32 = 0.05;
+
+    // Función para calcular la tarifa de transferencia
+    fn calculate_transfer_fee(&self, amount: u64) -> u64 {
+        (Self::TRANSFER_FEE_PERCENTAGE * amount as f32) as u64
+    }
+
     fn validate_transfer_amount(&self, amount: u64) -> Result<(), TransferError> {
         if amount <= 0 {
             return Err(TransferError::InvalidAmount);
         }
         Ok(())
     }
+
+    fn apply_transfer_fee(&self, amount: u64) -> u64 {
+        amount - self.calculate_transfer_fee(amount)
+    }
+
+    fn decrement_sender_balance(accounts: &mut Accounts, from: &String, amount: u64) -> Result<(), TransferError> {
+        accounts.adjust_balance(&from, -(amount as i64)).map_err(|e| match e {
+            AccountError::InsufficientBalance => TransferError::InsufficientBalance,
+            _ => panic!("Unexpected error"),
+        })
+    }
     
-    pub fn initiate_transfer(accounts: &mut Accounts, from: &String, to: &String, amount: u64, transfer_type: TransferType) -> Result<(), TransferError> {
-        // New call to validate the transfer amount
+    fn increment_recipient_balance(accounts: &mut Accounts, to: &String, amount: u64) -> Result<(), TransferError> {
+        accounts.adjust_balance(&to, amount as i64).map_err(|_| TransferError::AccountNotFound)
+    }
+
+    fn notify_user(user: &String, message: &str) {
+        println!("Notification to {}: {}", user, message);
+    }
+
+    pub fn initiate_transfer(&mut self, accounts: &mut Accounts, from: &String, to: &String, amount: u64, transfer_type: TransferType) -> Result<(), TransferError> {
         self.validate_transfer_amount(amount)?;
-        
-        // Validation of addresses
-        if !accounts.accounts_map.contains_key(from) {
+
+        let fee = self.calculate_transfer_fee(amount);
+        let final_amount = self.apply_transfer_fee(amount);
+
+        // Check if both sender and receiver exist before making any changes
+        if !accounts.account_exists(from) || !accounts.account_exists(to) {
             return Err(TransferError::AccountNotFound);
         }
-        if !accounts.accounts_map.contains_key(to) {
-            return Err(TransferError::AccountNotFound);
-        }
-        
-        // Verify that the sender has enough balance to make the transfer
-        if accounts.get_balance(&from)? < amount {
-            return Err(TransferError::InsufficientBalance);
-        }
 
-        // Decrement sender's balance
-        accounts.adjust_balance(&from, -(amount as i64))?;
-        
-        // Increment recipient's balance
-        accounts.adjust_balance(&to, amount as i64)?;
+        self.decrement_sender_balance(accounts, from, amount + fee)?; // Include fee in sender's decrement
+        self.increment_recipient_balance(accounts, to, final_amount)?;
 
-        // Record the transaction in the history of both accounts
         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
         let transaction = TransactionHistory {
             from: from.clone(),
             to: to.clone(),
-            amount,
+            amount: final_amount,
             timestamp,
         };
 
-        accounts.add_transaction_history(from, transaction.clone())?;
-        accounts.add_transaction_history(to, transaction.clone())?;
-        
-        // Store the transaction in the Transfer struct (New Line)
-        self.transactions.push(transaction);
+        accounts.add_transaction_history(from, transaction.clone()).unwrap();
+        accounts.add_transaction_history(to, transaction.clone()).unwrap();
 
-        // Log the transfer
-        log_transfer(from, to, amount, &transfer_type);
+        self.transactions.push(transaction);
+        log_transfer(from, to, final_amount, &transfer_type);
+
+        self.notify_user(from, &format!("You transferred {} coins to {}. Fee: {} coins.", final_amount, to, fee));
+        self.notify_user(to, &format!("You received {} coins from {}.", final_amount, from));
 
         Ok(())
     }
 
-    // New Function to revert a transaction
-    pub fn revert_transaction(&mut self, transaction_id: usize, caller: &String) -> Result<(), TransferError> {
+    pub fn revert_transaction(&mut self, accounts: &mut Accounts, transaction_id: usize, caller: &String) -> Result<(), TransferError> {
         let transaction = self.transactions.get(transaction_id).ok_or(TransferError::TransactionNotFound)?;
-        
+
+        // Check if the transaction was from the caller
         if &transaction.from != caller {
             return Err(TransferError::UnauthorizedRevert);
         }
-        
+
+        // Check if both sender and receiver exist before making any changes
+        if !accounts.account_exists(&transaction.from) || !accounts.account_exists(&transaction.to) {
+            return Err(TransferError::AccountNotFound);
+        }
+
         // Revert the transaction by adjusting the balances
-        accounts.adjust_balance(&transaction.from, transaction.amount as i64)?;
-        accounts.adjust_balance(&transaction.to, -(transaction.amount as i64))?;
+        self.decrement_sender_balance(accounts, &transaction.to, transaction.amount)?;  // Note the reversed roles
+        self.increment_recipient_balance(accounts, &transaction.from, transaction.amount)?;
 
         // Remove the transaction from the history
         self.transactions.remove(transaction_id);
-        
+
         Ok(())
     }
 
