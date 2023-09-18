201e6c3e1ca3d3b12820ded170d2c9cc25330c53
SirPotato27 <141689212+SirPotato27@users.noreply.github.com>
2023-09-17 22:28:30 -0500

    Update luks_main_contract.rs
    Update code 17/09/2023

--- Blockchain/Rust/MyLuks/src/luks_main_contract.rs
+++ Blockchain/Rust/MyLuks/src/luks_main_contract.rs
@@ -15,7 +15,9 @@ pub enum LuksError {
     AccountNotFound,
     InsufficientBalance,
     InvalidTransaction,
-    Unauthorized
+    Unauthorized,
+    CoinIssueError,
+    TransactionNotSecured,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -26,28 +28,44 @@ pub struct Address {
 #[derive(Debug, PartialEq)]
 pub struct Account {
     pub balance: u64,
-    // Additional details could go here (public keys, transaction history, etc.)
+    pub public_key: Option<String>, // For better security
+    // Other fields can be added for more features (transaction history, private keys if needed, etc.)
+}
+
+#[derive(Debug, PartialEq)]
+pub struct TransactionRecord {
+    from: Address,
+    to: Address,
+    amount: u64,
+    timestamp: u64, // Use a timestamp to record when the transaction occurred.
 }
 
 #[derive(Debug, PartialEq)]
 pub struct LuksMainContract {
     pub owner: Address,
     pub accounts: HashMap<Address, Account>,
-    // Adding a consensus property
     pub consensus_module: consensus::Consensus,
 }
 
 impl LuksMainContract {
-    // Function to create a new account
-    pub fn create_account(&mut self, address: Address, initial_balance: u64) -> Result<(), LuksError> {
+    
+    // 1. Historial de Transacciones
+    pub accounts_transaction_history: HashMap<Address, Vec<TransactionRecord>>,
+
+    // 2. Tarifas por Transacción
+    const TRANSACTION_FEE: u64 = 10; // Let's say a flat fee of 10 units.
+    pub fees_account: Address,
+    
+    /// Creates a new account with an optional public key.
+    pub fn create_account(&mut self, address: Address, initial_balance: u64, public_key: Option<String>) -> Result<(), LuksError> {
         if self.accounts.contains_key(&address) {
             return Err(LuksError::AccountAlreadyExists);
         }
-        self.accounts.insert(address, Account { balance: initial_balance });
+        self.accounts.insert(address, Account { balance: initial_balance, public_key });
         Ok(())
     }
 
-    // Function to delete an account
+    /// Deletes an account; only the contract owner can perform this action.
     pub fn delete_account(&mut self, address: &Address, caller: &Address) -> Result<(), LuksError> {
         if &self.owner != caller {
             return Err(LuksError::Unauthorized);
@@ -56,27 +74,95 @@ impl LuksMainContract {
         Ok(())
     }
 
-    // Function to get account details
+    /// Retrieves account details for a given address.
     pub fn get_account_details(&self, address: &Address) -> Option<&Account> {
         self.accounts.get(address)
     }
 
-    // Function to initiate a transfer
-    pub fn initiate_transfer(&mut self, from: &Address, to: &Address, amount: u64) -> Result<(), LuksError> {
+    /// Issues coins to a specified address.
+    pub fn issue_coins(&mut self, address: &Address, amount: u64) -> Result<(), LuksError> {
+        let account = self.accounts.get_mut(address).ok_or(LuksError::AccountNotFound)?;
+        account.balance += amount;
+        Ok(())
+    }
+
+    /// Initiates a transfer from one account to another, ensuring security checks and consensus validations.
+    pub fn initiate_transfer(&mut self, from: &Address, to: &Address, amount: u64, signature: String) -> Result<(), LuksError> {
+        if !security::is_transaction_secured(from, &signature) {
+            return Err(LuksError::TransactionNotSecured);
+        }
+
         let is_valid = validate_transaction::validate(from, to, amount)?;
-        if !is_valid {
+
+        if !is_valid || !self.consensus_module.validate_transaction(&from, &to, amount) {
             return Err(LuksError::InvalidTransaction);
         }
         
         let sender = self.accounts.get_mut(from).ok_or(LuksError::AccountNotFound)?;
         let receiver = self.accounts.get_mut(to).ok_or(LuksError::AccountNotFound)?;
-        
+
         if sender.balance < amount {
             return Err(LuksError::InsufficientBalance);
         }
+
+        sender.balance -= amount;
+        receiver.balance += amount;
+        Ok(())
+    }
+
+    // Retrieves transaction history for a given account.
+    pub fn get_transaction_history(&self, address: &Address) -> Option<&Vec<TransactionRecord>> {
+        self.accounts_transaction_history.get(address)
+    }
+
+    // 3. Cambio de Propietario
+    pub fn transfer_ownership(&mut self, new_owner: Address, caller: &Address) -> Result<(), LuksError> {
+        if &self.owner != caller {
+            return Err(LuksError::Unauthorized);
+        }
+        self.owner = new_owner;
+        Ok(())
+    }
+
+    // Enhanced transfer logic now with fees and transaction recording.
+    pub fn initiate_transfer(&mut self, from: &Address, to: &Address, amount: u64, signature: String) -> Result<(), LuksError> {
+        // ... (Validations remain the same)
+
+        let sender = self.accounts.get_mut(from).ok_or(LuksError::AccountNotFound)?;
+        let receiver = self.accounts.get_mut(to).ok_or(LuksError::AccountNotFound)?;
+
+        if sender.balance < amount + Self::TRANSACTION_FEE {
+            return Err(LuksError::InsufficientBalance);
+        }
+
+        // Deducting transaction fee
+        sender.balance -= Self::TRANSACTION_FEE;
+
+        // Add the transaction fee to the fees account
+        let fee_account = self.accounts.get_mut(&self.fees_account).ok_or(LuksError::AccountNotFound)?;
+        fee_account.balance += Self::TRANSACTION_FEE;
+
+        // Perform the actual transfer
         sender.balance -= amount;
         receiver.balance += amount;
 
+        // 1. Recording the transaction in the history
+        let record = TransactionRecord {
+            from: from.clone(),
+            to: to.clone(),
+            amount,
+            timestamp: self.get_current_timestamp(), // This is a placeholder; you would need to implement the timestamp retrieval.
+        };
+
+        self.accounts_transaction_history.entry(from.clone()).or_insert_with(Vec::new).push(record);
+
         Ok(())
+    }
+
+    // Placeholder function for getting current timestamp.
+    // You might want to use an external crate for this like 'chrono'.
+    fn get_current_timestamp(&self) -> u64 {
+        // Placeholder logic, not actual implementation
+        0
     }
 }
